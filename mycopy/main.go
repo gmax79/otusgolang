39d0b528781fe82fa84d47569c79b991dca02da7
Maksim Goncharenko <gm79@list.ru>
2019-10-12 16:02:07 +0300

    updated code in hw6 - mycopy

--- mycopy/main.go
+++ mycopy/main.go
@@ -12,33 +12,21 @@ func main() {
 
 	var from, to string
 	var offset, limit int64
-
 	flag.StringVar(&from, "from", "", "Source file path")
-	flag.StringVar(&to, "to", "", "Source file path")
-	flag.Int64Var(&offset, "offset", 0, "Start position offset [optional]")
-	flag.Int64Var(&limit, "limit", 0, "Limit copying data length [optional]")
+	flag.StringVar(&to, "to", "", "Destination file path")
+	flag.Int64Var(&offset, "offset", 0, "Start position offset [optional, > 0]")
+	flag.Int64Var(&limit, "limit", 0, "Limit copying data length [optional, > 0]")
 	flag.Parse()
-
-	if from == "" {
-		fmt.Println("Error: from parameter not declared")
-	}
-	if to == "" {
-		fmt.Println("Error: to parameter not declared")
-	}
-	if offset < 0 {
-		fmt.Println("Error: offset parameter in negative")
-	}
-	if limit < 0 {
-		fmt.Println("Error: limit parameter in negative")
-	}
 	if from == "" || to == "" || offset < 0 || limit < 0 {
-		fmt.Println("Program copies part of file to another file.")
-		fmt.Println("Use --help option to see options list.")
+		fmt.Println("Error: Set invalid parameters")
+		flag.Usage()
 		return
 	}
 	err := copy(from, to, offset, limit)
 	if err != nil {
 		fmt.Println("Error:", err.Error())
+	} else {
+		fmt.Println("")
 	}
 }
 
@@ -68,22 +56,12 @@ func copy(from, to string, offset, limit int64) error {
 		return err
 	}
 	defer dst.Close()
-	copied, err := copyio(src, dst, limit)
-	if err != nil {
-		return err
-	}
-
-	var percents float64
-	if srcsize > 0 {
-		percents = float64(copied) / float64(srcsize)
-		percents *= 100
-	}
-	fmt.Printf("Copied %d bytes (%f percents)\n", copied, percents)
-	return nil
+	_, err = copyio(src, dst, limit, srcsize)
+	return err
 }
 
 // copyio - function to run tests
-func copyio(src io.Reader, dst io.Writer, limit int64) (copied int64, err error) {
+func copyio(src io.Reader, dst io.Writer, limit, size int64) (copied int64, err error) {
 	buffer := make([]byte, 16384)
 	for {
 		var readed int
@@ -98,6 +76,13 @@ func copyio(src io.Reader, dst io.Writer, limit int64) (copied int64, err error)
 				return
 			}
 			copied += readed64
+			var percents float64
+			if size > 0 {
+				percents = float64(copied) / float64(size)
+				percents *= 100
+			}
+			fmt.Printf("\033[2K\rProgress: %.2f%%", percents) // Inplace print
+			//time.Sleep(5 * time.Millisecond)                // Demonstration delay
 			if limit > 0 {
 				limit -= readed64
 				if limit == 0 {
